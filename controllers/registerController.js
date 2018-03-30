f37dc54e3937d03ea4ebd52dde80f4aa979c8ae3
madhuripatil506 <madhuripatil506@gmail.com>
2018-03-30 19:19:33 +0530

    commit

---
+++ controllers/registerController.js
@@ -0,0 +1,8 @@
+var app = angular.module('myApp');
+app.controller('registercontroller', function($scope)
+{
+  // $scope.firstname = "";
+  // $scope.lastname = "";
+  $scope.email = "";
+  $scope.password="";
+});
